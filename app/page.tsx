ab740915358a5b00cf73e39db27db80a94301118
Oscar Harron <oscarharron@hotmail.com>
2025-02-15 10:53:54 +1300

    add update message for broken api

--- app/page.tsx
+++ app/page.tsx
@@ -2,63 +2,73 @@ import Fights from './components/fights';
 import { randomIntFromInterval } from './helpers/randomIntFromInterval';
 import { getFighterFromJson } from './helpers/getFighterFromJson';
 import { getAllFightersFromJson } from './helpers/getAllFightersFromJson';
-async function getFighterData() {
-  const fighter = await getFighterFromJson();
-  const res = await fetch(
-    `https://${process.env.RAPID_API_HOST}/Events/FindEventsByFighterName/${fighter.fighter}?limit=30`,
-    {
-      next: { revalidate: 43200 },
-      headers: {
-        'Content-Type': 'application/json',
-        'x-rapidapi-key': process.env.RAPID_API_KEY
-          ? process.env.RAPID_API_KEY
-          : '',
-        'x-rapidapi-host': process.env.RAPID_API_HOST
-          ? process.env.RAPID_API_HOST
-          : '',
-      },
-    }
-  );
+// async function getFighterData() {
+//   const fighter = await getFighterFromJson();
+//   const res = await fetch(
+//     `https://${process.env.RAPID_API_HOST}/Events/FindEventsByFighterName/${fighter.fighter}?limit=30`,
+//     {
+//       next: { revalidate: 43200 },
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'x-rapidapi-key': process.env.RAPID_API_KEY
+//           ? process.env.RAPID_API_KEY
+//           : '',
+//         'x-rapidapi-host': process.env.RAPID_API_HOST
+//           ? process.env.RAPID_API_HOST
+//           : '',
+//       },
+//     }
+//   );
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
+//   if (!res.ok) {
+//     console.log(res);
+//     throw new Error('Failed to fetch data');
+//   }
 
-  const data = await res.json();
-  const fightInfo = filterFights(data, fighter.fighter);
+//   const data = await res.json();
+//   const fightInfo = filterFights(data, fighter.fighter);
 
-  return { fightInfo, fighter };
-}
+//   return { fightInfo, fighter };
+// }
 
-function filterFights(data: any, fighter: string) {
-  let max = data.length - 5;
-  let randomNumber = randomIntFromInterval(0, max);
+// function filterFights(data: any, fighter: string) {
+//   let max = data.length - 5;
+//   let randomNumber = randomIntFromInterval(0, max);
 
-  let fivefights = data
-    .slice(randomNumber, randomNumber + 5)
-    .map((f: any) => ({
-      fighter: fighter,
-      date: f.Date,
-      fighterOne: f['Fighter 1'],
-      fighterTwo: f['Fighter 2'],
-      winner: f.Winner,
-      weightClass: f['Weight_Class'],
-    }))
-    .reverse();
+//   let fivefights = data
+//     .slice(randomNumber, randomNumber + 5)
+//     .map((f: any) => ({
+//       fighter: fighter,
+//       date: f.Date,
+//       fighterOne: f['Fighter 1'],
+//       fighterTwo: f['Fighter 2'],
+//       winner: f.Winner,
+//       weightClass: f['Weight_Class'],
+//     }))
+//     .reverse();
 
-  return fivefights;
-}
+//   return fivefights;
+// }
 
 export default async function Home() {
-  const { fightInfo, fighter } = await getFighterData();
-  const allFighters = await getAllFightersFromJson();
+  // const { fightInfo, fighter } = await getFighterData();
+  // const allFighters = await getAllFightersFromJson();
 
   return (
-    <Fights
-      fightInfo={fightInfo}
-      fighterName={fighter.fighter}
-      fighterNation={fighter.nation}
-      allFighters={allFighters}
-    />
+    <div>
+      <h2>Update</h2>
+      <p>
+        We are currently experiencing some issues with our API
+        provider. <br />
+        We will be back as soon as possible!
+      </p>
+    </div>
+
+    // <Fights
+    //   fightInfo={fightInfo}
+    //   fighterName={fighter.fighter}
+    //   fighterNation={fighter.nation}
+    //   allFighters={allFighters}
+    // />
   );
 }
